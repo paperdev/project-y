a7c6397781d012536ba26ba9d99c6ebde794d9c1
Kevin <paperdev0@gmail.com>
2024-03-05 14:51:34 +0900

    sub title added

--- src/components/template/header.tsx
+++ src/components/template/header.tsx
@@ -1,15 +1,20 @@
 'use client';
 
-import React from 'react';
+import React, { useContext } from 'react';
 import { ThemeSwitcher } from '@/components/template/ThemeSwitcher';
 import { RegionSelector } from '@/components/RegionSelector';
-import { IonHeader, IonTitle, IonToolbar } from '@ionic/react';
+import { IonHeader, IonText, IonTitle, IonToolbar } from '@ionic/react';
 import ComponentSearchInput from '@/components/(youtube)/(search)/searchInput';
 import ComponentVideoCategory from '@/components/(youtube)/videoCategory';
 import { usePathname } from 'next/navigation';
+import { QueryContext } from '@/app/providers';
+
+const displayRegionName = new Intl.DisplayNames(['EN'], { type: 'region' });
 
 export default function Header() {
   const pathname = usePathname();
+  const query = useContext(QueryContext);
+  const regionCode = query.regionCode;
 
   return (
     <IonHeader collapse='condense'>
@@ -17,7 +22,12 @@ export default function Header() {
         
         <RegionSelector />
         
-        <IonTitle color='primary' >Trend Insight</IonTitle>
+        <IonTitle color='primary' >
+          <IonText>Trend Insight</IonText>
+          <p>
+            <IonText color={'secondary'} className='text-xs'>{displayRegionName.of(regionCode)}</IonText>
+          </p>
+        </IonTitle>
 
         <ThemeSwitcher />
 
