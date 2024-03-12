c79ac1bb6116f0f9303d54e1bbe3103e4602fabf
Kevin <paperdev0@gmail.com>
2024-03-12 16:09:00 +0900

    browser fixed

--- src/app/(pages)/about/licenses.tsx
+++ src/app/(pages)/about/licenses.tsx
@@ -3,6 +3,7 @@
 import React from 'react';
 import { IonBackButton, IonButton, IonButtons, IonContent, IonHeader, IonText, IonTitle, IonToolbar } from '@ionic/react';
 import Link from 'next/link';
+import { Browser } from '@capacitor/browser';
 
 export default function LicensesPage() {
   return (
@@ -20,20 +21,35 @@ export default function LicensesPage() {
         <IonText className='p-10'>
           <li>
             Audio streaming data developed with YouTube.com are licensed by 
-            <Link href='https://developers.google.com/youtube/terms/api-services-terms-of-service'>
-              <IonText color={'primary'}> YouTube API Services Terms of Service </IonText>
-            </Link>
-            and 
-            <Link href='https://www.youtube.com/t/terms'>
-              <IonText color={'primary'}> YouTube Terms of Service.</IonText>
-            </Link>
+            <IonText 
+              color={'primary'}
+              onClick={() => {
+                Browser.open({
+                  url: 'https://developers.google.com/youtube/terms/api-services-terms-of-service',
+                });
+              }}
+            > YouTube API Services Terms of Service </IonText>
+            and
+            <IonText 
+              color={'primary'}
+              onClick={() => {
+                Browser.open({
+                  url: 'https://www.youtube.com/t/terms',
+                });
+              }}
+            > YouTube Terms of Service.</IonText>
           </li>
           <br />
           <li>
             YouTube-Player-iOS-Helper made by YouTube is licensed by
-            <Link href='https://www.apache.org/licenses/LICENSE-2.0'>
-              <IonText color={'primary'}> Apache License, Version 2.0.</IonText>
-            </Link>
+            <IonText 
+              color={'primary'}
+              onClick={() => {
+                Browser.open({
+                  url: 'https://www.apache.org/licenses/LICENSE-2.0',
+                });
+              }}
+            > Apache License, Version 2.0.</IonText>
           </li>
         </IonText>
       </IonContent>
