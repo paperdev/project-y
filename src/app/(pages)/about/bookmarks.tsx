c90efa6064586337ea3d7be0987e2bd0c286d10b
Kevin <paperdev0@gmail.com>
2024-03-15 11:19:52 +0900

    bookmarks updated

--- src/app/(pages)/about/bookmarks.tsx
+++ src/app/(pages)/about/bookmarks.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React, { useEffect, useRef, useState } from 'react';
-import { IonBackButton, IonButtons, IonContent, IonHeader, IonIcon, IonItem, IonItemOption, IonItemOptions, IonItemSliding, IonList, IonText, IonTitle, IonToolbar } from '@ionic/react';
+import { IonBackButton, IonButtons, IonContent, IonHeader, IonIcon, IonItem, IonItemDivider, IonItemGroup, IonItemOption, IonItemOptions, IonItemSliding, IonLabel, IonList, IonText, IonTitle, IonToolbar } from '@ionic/react';
 import { iBookmark } from '@/shared/interface/bookmark';
 import { trash } from 'ionicons/icons';
 import { Preferences } from '@capacitor/preferences';
@@ -26,12 +26,16 @@ const getBookmarkList = async () => {
 }
 
 export default function BookmarksPage() {
-  const [bookmarkList, setBookmarkList] = useState<iBookmark[]>([]);
+  const [bookmarkList, setBookmarkList] = useState<Record<string, iBookmark[]>>();
   const listRef = useRef<HTMLIonListElement>(null);
 
-  const onClickDelete = async (index: number) => {
-    const temp: iBookmark[] = Array.from(bookmarkList);
-    const [item] = temp.splice(index, 1);
+  const onClickDelete = async (key: string, index: number) => {
+    
+    const temp = Object.assign({}, bookmarkList);
+    const [item] = temp[key].splice(index, 1);
+    if (0 === temp[key].length) {
+      delete temp[key];
+    }
     setBookmarkList(temp);
 
     listRef?.current?.closeSlidingItems();
@@ -47,10 +51,48 @@ export default function BookmarksPage() {
 
   useEffect(() => {
     if (!data || 0 === data.length) {
-      return
+      return;
     }
 
-    setBookmarkList(data);
+    const sortedList = data.sort(function (a: iBookmark, b: iBookmark) {
+      const tempA = a.group;
+      const tempB = b.group;
+      if (tempA < tempB) {
+        return -1;
+      }
+      if (tempB > tempA) {
+        return 1;
+      }
+      return 0;
+    });
+
+    let groupedList: Record<string, iBookmark[]> = {};
+    sortedList.map((item: iBookmark) => {
+      if (groupedList[item.group]) {
+        groupedList[item.group].push(
+          {
+            id: item.id,
+            group: item.group,
+            name: item.name,
+            url: item.url,
+            timestamp: item.timestamp,
+          }
+        )
+      }
+      else {
+        groupedList[item.group] = [
+          {
+            id: item.id,
+            group: item.group,
+            name: item.name,
+            url: item.url,
+            timestamp: item.timestamp,
+          }
+        ]
+      }
+    });
+
+    setBookmarkList(groupedList);
   }, [data]);
   
   return (
@@ -65,33 +107,42 @@ export default function BookmarksPage() {
       </IonHeader>
 
       <IonContent className='ion-padding-top ion-padding-bottom'>
-        {
-          0 !== bookmarkList.length && 
-            <IonList ref={listRef}>
-              {
-                bookmarkList.map((item: iBookmark, index: number) => {
-                  return (
-                    <IonItemSliding key={index}>
-                      <IonItem>
-                        <IonText className='truncate overflow-hidden'>
-                          {item.name}
-                        </IonText>
-                      </IonItem>
+        <IonList ref={listRef}>
+          {
+            bookmarkList && 
+            Object.keys(bookmarkList).map((key: string) => {
+              return (
+                <IonItemGroup key={key}>
+                  <IonItemDivider color={'medium'} className='opacity-70'>
+                    <IonLabel>{key}</IonLabel>
+                  </IonItemDivider>
+                  {
+                    bookmarkList[key].map((item: iBookmark, index: number) => {
+                      return (
+                        <IonItemSliding key={index}>
+                          <IonItem>
+                            <IonText className='truncate overflow-hidden'>
+                              {item.name}
+                            </IonText>
+                          </IonItem>
 
-                      <IonItemOptions>
-                        <IonItemOption
-                          color='danger'
-                          onClick={() => {onClickDelete(index)}}
-                        >
-                          <IonIcon slot='icon-only' icon={trash} />
-                        </IonItemOption>
-                      </IonItemOptions>
-                    </IonItemSliding>
-                  )
-                })
-              }
-            </IonList>
-        }
+                          <IonItemOptions>
+                            <IonItemOption
+                              color='danger'
+                              onClick={() => {onClickDelete(key, index)}}
+                            >
+                              <IonIcon slot='icon-only' icon={trash} />
+                            </IonItemOption>
+                          </IonItemOptions>
+                        </IonItemSliding>
+                      )
+                    })
+                  }
+                </IonItemGroup>
+              )
+            })
+          }
+        </IonList>
       </IonContent>
     </>
   );
